779bd51831430b214a73f187c8bdae43818ddc9b
胖橙 <3120154500@qq.com>
2022-12-20 17:18:31 +0800

    abstract learn

---
+++ src/stract/Person.java
@@ -0,0 +1,10 @@
+package stract;
+
+public abstract class Person {
+    public void getInfo(){
+        System.out.println("My name is ...");
+    }
+
+    public abstract void run();
+    public abstract void say();
+}
---
+++ src/stract/Student.java
@@ -0,0 +1,11 @@
+package stract;
+
+public class Student extends Person{
+    public void say(){
+        System.out.println("I am a student");
+    }
+
+    public void run(){
+        System.out.println("I can run");
+    }
+}
